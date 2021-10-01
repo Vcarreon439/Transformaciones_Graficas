f377e9702205b9315f9e2ff759cc4e9d82059522
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2021-09-30 23:33:23 -0500

    Cambios para identificacion de TipoDFigura

--- Transformaciones_Graficas/ClasesFiguras/Elipse.cs
+++ Transformaciones_Graficas/ClasesFiguras/Elipse.cs
@@ -9,20 +9,29 @@ namespace Transformaciones_Graficas
 {
     public class Elipse : Figura
     {
+        public Elipse(Figura fig) : base(fig)
+        {
+            this.TipoDFigura = TipodeFigura.Elipse;
+        }
+
         public Elipse(Point origin, Point end) : base(origin, end)
         {
+            this.TipoDFigura = TipodeFigura.Elipse;
         }
 
         public Elipse(Point origin, Point end, Pen contorno) : base(origin, end, contorno)
         {
+            this.TipoDFigura = TipodeFigura.Elipse;
         }
 
         public Elipse(Point origin, Point end, SolidBrush relleno) : base(origin, end, relleno)
         {
+            this.TipoDFigura = TipodeFigura.Elipse;
         }
 
         public Elipse(Point origin, Point end, Pen contorno, SolidBrush relleno) : base(origin, end, contorno, relleno)
         {
+            this.TipoDFigura = TipodeFigura.Elipse;
         }
     }
 }
