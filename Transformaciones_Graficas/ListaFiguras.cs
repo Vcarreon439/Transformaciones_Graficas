9a340b13330105302ad841c3395b1d9e80cd2598
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2021-09-30 23:35:02 -0500

    Lista personalizada para lograr que los trazos se acumulen en el grafico.

---
+++ Transformaciones_Graficas/ListaFiguras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transformaciones_Graficas
+{
+    public class ListaFiguras
+    {
+        public List<Figura> enCanva;
+
+        public ListaFiguras()
+        {
+            enCanva = new List<Figura>();
+        }
+
+        public void AgregarFigura(Figura figura)
+        {
+            enCanva.Add(figura);
+        }
+
+        public void QuitarFigura(Figura figura)
+        {
+            enCanva.Remove(figura);
+        }
+
+    }
+}
